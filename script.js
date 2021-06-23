d5974e29676614832871c9ecf4e1b6a4f11caa2a
nenad <c.nenad00@gmail.com>
2021-06-23 20:22:11 +0100

    add slider functionallity

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 'use strict';
 
-// Variables
+// // Variables
+// Menu
 const header = document.querySelector('header');
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.menu');
 const logo = document.querySelector('.logo');
 
-//Slider
+// Slider
 const images = document.querySelectorAll('.images-container picture');
 const captions = document.querySelectorAll('.caption');
-const currentSlide = 0;
-const maxSlide = images.length;
+const sliderLeft = document.querySelector('.controls-left');
+const sliderRight = document.querySelector('.controls-right');
+let currentSlide = 0;
 
 // //Functionality
 
 // Menu
-const checkScreenSize = function () {
+const showMenu = function () {
   if (window.innerWidth <= 800) {
     menu.classList.add('hidden');
   } else {
     menu.classList.remove('hidden');
   }
 };
-checkScreenSize();
+showMenu();
 
 const changeMenuIcon = function () {
   if (header.classList.contains('open')) {
@@ -32,7 +34,7 @@ const changeMenuIcon = function () {
   }
 };
 
-const mobileMenu = function () {
+const openMenu = function () {
   changeMenuIcon();
   logo.classList.toggle('hidden');
   menu.classList.toggle('hidden');
@@ -40,6 +42,32 @@ const mobileMenu = function () {
 };
 
 //Slider
+const initSlideDisplay = function (n) {
+  images.forEach(image => {
+    image.style.opacity = '0';
+  });
+
+  captions.forEach(caption => {
+    caption.style.opacity = '0';
+  });
+
+  images[n].style.opacity = 1;
+  captions[n].style.opacity = 1;
+};
+
+const nextSlide = () => {
+  currentSlide >= images.length - 1 ? (currentSlide = 0) : currentSlide++;
+  initSlideDisplay(currentSlide);
+};
+
+const prevSlide = () => {
+  currentSlide <= 0 ? (currentSlide = images.length - 1) : currentSlide--;
+  initSlideDisplay(currentSlide);
+};
 
 // Event handlers
-hamburgerBtn.addEventListener('click', mobileMenu);
+hamburgerBtn.addEventListener('click', openMenu);
+document.addEventListener('DOMContentLoaded', initSlideDisplay(currentSlide));
+sliderRight.addEventListener('click', nextSlide);
+sliderLeft.addEventListener('click', prevSlide);
+setInterval(nextSlide, 5000);
